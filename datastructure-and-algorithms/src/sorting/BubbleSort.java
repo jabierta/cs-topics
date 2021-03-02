59b59a5eda5825fdab7ff2104c5bb143aa8aeaaa
johndheem <johndheem@blendernetworks.com>
2021-03-01 23:15:15 -0400

    Going to start sorting algorithms

---
+++ datastructure-and-algorithms/src/sorting/BubbleSort.java
@@ -0,0 +1,12 @@
+package src.sorting;
+
+public class BubbleSort {
+    // method for sorting array
+    // method for sorting linked list
+    // sort queue
+    // sort stack
+    // sort trees
+    // sorting graphs
+
+}
+// https://www.tutorialspoint.com/data_structures_algorithms/insertion_sort_algorithm.htm
