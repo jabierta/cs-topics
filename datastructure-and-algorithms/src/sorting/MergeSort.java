b7fc7f8440a4f484813e1fd0e2e19a7c199164ec
johndheem <johndheem@blendernetworks.com>
2021-03-04 07:51:49 -0400

    MergeSort Complete for Integer Arrays

--- datastructure-and-algorithms/src/sorting/MergeSort.java
+++ datastructure-and-algorithms/src/sorting/MergeSort.java
@@ -1,5 +1,58 @@
 package src.sorting;
 
-public class MergeSort {
+import src.linkedlist.SingleLinkedList;
 
+public class MergeSort {
+    /**
+     * Time Complexity -> Worst Case: n log n, Average Case: n log n
+     */
+    public Integer[] sortArray(Integer[] array) {
+        return this.mergeSortArray(array);
+    }
+
+    private Integer[] mergeSortArray(Integer[] array) {
+        if (array.length <= 1) {
+            return array;
+        }
+
+        Integer[] leftArray = new Integer[(array.length + 1) / 2];
+        Integer[] rightArray = new Integer[(array.length - leftArray.length)];
+
+        for (int i = 0; i < array.length; i++) {
+            if (i < leftArray.length)
+                leftArray[i] = array[i];
+            else
+                rightArray[i - leftArray.length] = array[i];
+        }
+
+        return mergeArrays(mergeSortArray(leftArray), mergeSortArray(rightArray));
+    }
+
+    private Integer[] mergeArrays(Integer[] array1, Integer[] array2) {
+        int size = array1.length + array2.length;
+        Integer[] array = new Integer[size];
+        int indexAtArray1 = 0;
+        int indexAtArray2 = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (indexAtArray1 >= array1.length) {
+                array[i] = array2[indexAtArray2];
+                indexAtArray2++;
+            } else if (indexAtArray2 >= array2.length) {
+                array[i] = array1[indexAtArray1];
+                indexAtArray1++;
+            } else if (array1[indexAtArray1] <= array2[indexAtArray2]) {
+                array[i] = array1[indexAtArray1];
+                indexAtArray1++;
+            } else {
+                array[i] = array2[indexAtArray2];
+                indexAtArray2++;
+            }
+        }
+
+        return array;
+    }
+
+    public SingleLinkedList sortSingleLinkedList() {
+        return null;
+    }
 }
--- datastructure-and-algorithms/src/sorting/SortDriver.java
+++ datastructure-and-algorithms/src/sorting/SortDriver.java
@@ -7,35 +7,42 @@ public class SortDriver {
     public void run() {
         System.out.println("Sorting Algorithms");
         System.out.println("NOTE: All Sorting algorithms are sorting in ascending direction!");
-        Random random = new Random();
+        
+        Integer[] unsortedArray = this.generateValuesForIntegerArray();
 
-        Integer[] unsortedArray = new Integer[random.nextInt(50)];
-
-        for (int i = 0; i < unsortedArray.length; i++) {
-            unsortedArray[i] = random.nextInt(100);
-        }
-
-        // System.out.println("========================BUBBLE SORT========================");
-        // BubbleSort bubbleSort = new BubbleSort();
-        // System.out.println("Unsorted Array: ");
-        // this.print(unsortedArray);
-        // System.out.println("\nSorted Array: ");
-        // this.print(bubbleSort.sortArray(unsortedArray));
-        // System.out.println("\n========================BUBBLE SORT========================");
+        System.out.println("========================BUBBLE SORT========================");
+        BubbleSort bubbleSort = new BubbleSort();
+        System.out.println("Unsorted Array: ");
+        this.print(unsortedArray);
+        System.out.println("\nSorted Array: ");
+        this.print(bubbleSort.sortArray(unsortedArray));
+        System.out.println("\n========================BUBBLE SORT========================");
 
         System.out.println("\n========================QUICK SORT========================");
         QuickSort quickSort = new QuickSort();
-        System.out.println("Unsorted Array: ");
+        // TODO Optimize Quick Sort!
+        // unsortedArray = this.generateValuesForIntegerArray();
         unsortedArray = new Integer[] {5, 3, 1, 4, 6};
+        System.out.println("Unsorted Array: ");
         this.print(unsortedArray);
         System.out.println("\nSorted Array: ");
         this.print(quickSort.sortArray(unsortedArray));
         System.out.println("\n========================QUICK SORT========================");
-            
+
+
+        System.out.println("\n========================MERGE SORT========================");
+        MergeSort mergeSort = new MergeSort();
+        unsortedArray = this.generateValuesForIntegerArray();
+        System.out.println("Unsorted Array: ");
+        this.print(unsortedArray);
+        System.out.println("\nSorted Array: ");
+        this.print(mergeSort.sortArray(unsortedArray));
+        System.out.println("\n========================MERGE SORT========================");
+
 
     }
 
-    private void print(Object[] array) {
+    private void print(Integer[] array) {
         System.out.print("[");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i].toString());
@@ -44,5 +51,14 @@ public class SortDriver {
             }
         }
         System.out.print("]");
+    }c
+
+    private Integer[] generateValuesForIntegerArray() {
+        Random random = new Random();
+        Integer[] unsortedArray = new Integer[random.nextInt(50)];
+        for (int i = 0; i < unsortedArray.length; i++) {
+            unsortedArray[i] = random.nextInt(100);
+        }
+        return unsortedArray;
     }
 }
