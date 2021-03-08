774eda456b0b1c6970d7f7b9e930b89be316e8ab
johndheem <johndheem@blendernetworks.com>
2021-03-08 09:15:52 -0400

    Need to apply the methods

---
+++ datastructure-and-algorithms/src/dynamicprogramming/CountThePaths.java
@@ -0,0 +1,21 @@
+package src.dynamicprogramming;
+
+public class CountThePaths {
+    // Suppose with that with an n * n grid
+    // With movements limited to : down and right
+    // Starting point 0,0 Finishing point n,n
+    // Find how many paths
+    // Note that an index i and index j could be blocked off such that it cannot be used as part of
+    // the path
+    // Return how many paths are available from the starting point to the finish point
+    // path is a set of indices that connect sp and fp
+
+    // Recursive Solution O(2^n^2)
+
+    // Dynamic Solution Memoization O(n^2)
+
+    // Dynamic Solution Tabulation O(n)
+
+    // https://youtu.be/P8Xa2BitN3I?t=532
+    // https://www.geeksforgeeks.org/tabulation-vs-memoization/
+}
