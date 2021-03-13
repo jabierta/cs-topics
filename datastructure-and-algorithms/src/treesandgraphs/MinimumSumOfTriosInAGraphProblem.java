54d0cf2b5402f1a963e28dcad37f3ec8ef1eba84
johndheem <johndheem@blendernetworks.com>
2021-03-12 21:41:33 -0400

    Trying to relearn graphs

---
+++ datastructure-and-algorithms/src/treesandgraphs/MinimumSumOfTriosInAGraphProblem.java
@@ -0,0 +1,21 @@
+package src.treesandgraphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinimumSumOfTriosInAGraphProblem {
+
+
+    public void buildGraph() {
+
+    }
+
+    public int getMinimumSumOfTrios(int totalNumNodes, int[] nodesFrom, int[] nodesTo) {
+        return 0;
+    }
+
+    class Node {
+        int id;
+        List<Node> adjacentNodes = new ArrayList<>();
+    }
+}
